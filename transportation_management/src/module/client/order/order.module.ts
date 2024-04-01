409427881da47ed7b15d85c5e93abc9f1ca74084
Duan Nguen Van <oneProject/source>
2024-04-01 18:04:22 +0700

    !

--- transportation_management/src/module/client/order/order.module.ts
+++ transportation_management/src/module/client/order/order.module.ts
@@ -1,9 +1,25 @@
 import { Module } from '@nestjs/common';
 import { OrderService } from './order.service';
 import { OrderController } from './order.controller';
+import { ConfigModule } from '@nestjs/config';
+import { JwtModule } from '@nestjs/jwt';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { AccountEntity } from 'src/entities/account.entity';
+import { AddressEntity } from 'src/entities/address.entity';
+import { CustomerEntity } from 'src/entities/customer.entity';
 
 @Module({
+    imports: [
+        TypeOrmModule.forFeature([CustomerEntity, AddressEntity, AccountEntity]),
+        ConfigModule,
+        JwtModule.register({
+            global: true,
+            secret: process.env.SECRET_KEY,
+            signOptions: { expiresIn: process.env.EXPIRES_IN_TOKEN },
+        }),
+    ],
     controllers: [OrderController],
     providers: [OrderService],
+    exports: [TypeOrmModule],
 })
 export class OrderModule {}
