4967be2d0a640c206bdc0102d50c54bc830d2a79
Duan Nguen Van <oneProject/source>
2024-04-18 14:12:35 +0700

    .

--- transportation_management/src/app.service.ts
+++ transportation_management/src/app.service.ts
@@ -1,10 +1,8 @@
 import { Injectable } from '@nestjs/common';
-import * as dotenv from 'dotenv';
 
 @Injectable()
 export class AppService {
     getHello(): any {
-
         return 'Hello DuanNV!:';
     }
 }
